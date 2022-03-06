738a147d60d404933e0cd5e37317ab25b3779c77
Fens05 <fens05m@gmail.com>
2022-03-05 20:18:11 -0600

    two

--- fens-km-prortfolio/src/App.js
+++ fens-km-prortfolio/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
+import React, {useState } from "react";
+import {render} from "react-dom";
 import './App.css';
-import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.sss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {button} from 'react-bootstrap';
+function App(){
+  return <div className="App">
+    <header className="App-header">
+      <Button>Test Button</Button>
+    </header>
+  </div>
 }
 
 export default App;
--- fens-km-prortfolio/src/components/About/index.js
+++ fens-km-prortfolio/src/components/About/index.js
@@ -1,19 +1,1 @@
-import React from 'react';
-import coverImage from '../../assests/images';
 
-function About() {
-    return (
-        <section className="row ">
-            <h1 id="about">About Me</h1>
-            <img src={coverImage} className=""></img>
-            <div className="col md">
-                <p>
-                    Hello my name is Krista. I have a general knowlege of front end framework.
-                    I love to dable in color schemes and layouts. 
-                </p>
-            </div>
-
-        </section>
-    );
-}
-export default About;
